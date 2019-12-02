0c5733250cb5101c1b91b2371cb099adb1d42809
Austin Shoecraft <ashoecraft@jibe.com>
2019-12-02 10:32:28 -0500

    created space for day 2

---
+++ two_a/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let file = File::open("input.txt")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+
+
+}
