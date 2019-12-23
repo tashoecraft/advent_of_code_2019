215cc8231c70a724f21df202565189612a38f299
Austin Shoecraft <austin@shoecraft.xyz>
2019-12-23 14:11:28 -0500

    finished 3 a

--- three_a/src/main.rs
+++ three_a/src/main.rs
@@ -2,16 +2,15 @@ use std::fs;
 use std::collections::HashSet;
 
 fn main() -> std::io::Result<()> {
-    let mut v: Vec<String> = fs::read_to_string("input.txt")?.split("\n").map(|item| {item.to_string()}).collect();
-    let mut wire_one: Vec<String> = v[0].split(",").map(|item| {item.to_string()}).collect();
-    let mut wire_two: Vec<String> = v[1].split(",").map(|item| {item.to_string()}).collect();
+    let v: Vec<String> = fs::read_to_string("input.txt")?.split("\n").map(|item| {item.to_string()}).collect();
+    let wire_one: Vec<String> = v[0].split(",").map(|item| {item.to_string()}).collect();
+    let wire_two: Vec<String> = v[1].split(",").map(|item| {item.to_string()}).collect();
 //    let mut test = HashSet::new();
 
     let wire_one_coordinates: HashSet<String> = convert_to_coordinates(&wire_one);
     let wire_two_coordinates: HashSet<String> = convert_to_coordinates(&wire_two);
 
-    let collisions = wire_two_coordinates.intersection(&wire_one_coordinates);
-
+    let collisions: i32 = find_min(wire_two_coordinates.intersection(&wire_one_coordinates));
     println!("{:?}", collisions);
     Ok(())
 
@@ -28,47 +27,79 @@ fn main() -> std::io::Result<()> {
 * Go through series for second wire, check if value is in hash of first
 * if so mark it.
 * Loop through hits and find one with value closet to 0,0
+* 
+* r8
 *
 */
 fn convert_to_coordinates(directions: &Vec<String>) -> HashSet<String> {
     let mut wire_tracker = [0,0];
     let mut all_touched_coordinates: HashSet<String> = HashSet::new();
     for item in directions.iter() {
-        let direction = &item[0..1];
+        let test = item.trim();
+
+        let direction = &test[0..1];
 
         if direction == "U" {
-            let original_coordinate = wire_tracker[1];
-            wire_tracker[1] += &item[1..].parse::<i32>().unwrap();
 
-            for i in original_coordinate..wire_tracker[1] {
-                let xy: Vec<String> = vec![wire_tracker[0], i].into_iter().map(|item| {item.to_string()}).collect();
-                all_touched_coordinates.insert(xy.join());
-            }
-        } else if direction == "R" {
             let original_coordinate = wire_tracker[1];
-            wire_tracker[0] += &item[1..].parse::<i32>().unwrap();
-            for i in original_coordinate..wire_tracker[1] {
+            wire_tracker[1] += &test[1..].parse::<i32>().unwrap();
+            for i in original_coordinate+1..wire_tracker[1]+1 {
                 let xy: Vec<String> = vec![wire_tracker[0], i].into_iter().map(|item| {item.to_string()}).collect();
-                all_touched_coordinates.insert(xy.join());
+                all_touched_coordinates.insert(xy.join(","));
             }
+
+        } else if direction == "R" {
+            let original_coordinate = wire_tracker[0];
+            wire_tracker[0] += &test[1..].parse::<i32>().unwrap();
+
+            for i in original_coordinate..wire_tracker[0]+1 {
+                let xy: Vec<String> = vec![i, wire_tracker[1]].into_iter().map(|item| {item.to_string()}).collect();
+                all_touched_coordinates.insert(xy.join(","));
+            }
+            // println!("{:?}", all_touched_coordinates);
+
         } else if direction == "D" {
             let original_coordinate = wire_tracker[1];
-            wire_tracker[1] -= &item[1..].parse::<i32>().unwrap();
-            for i in original_coordinate..wire_tracker[1] {
+            wire_tracker[1] -= &test[1..].parse::<i32>().unwrap();
+            // println!("{:?}", original_coordinate..wire_tracker[1]);
+
+            for i in wire_tracker[1]..original_coordinate {
                 let xy: Vec<String> = vec![wire_tracker[0], i].into_iter().map(|item| {item.to_string()}).collect();
-                all_touched_coordinates.insert(xy.join());
+                all_touched_coordinates.insert(xy.join(","));
             }
         } else if direction == "L" {
-            let original_coordinate = wire_tracker[1];
-            wire_tracker[0] -= &item[1..].parse::<i32>().unwrap();
-            for i in original_coordinate..wire_tracker[1] {
-                let xy: Vec<String> = vec![wire_tracker[0], i].into_iter().map(|item| {item.to_string()}).collect();
-                all_touched_coordinates.insert(xy.join());
+            let original_coordinate = wire_tracker[0];
+            wire_tracker[0] -= &test[1..].parse::<i32>().unwrap();
+            for i in wire_tracker[0]..original_coordinate {
+                let xy: Vec<String> = vec![i, wire_tracker[1]].into_iter().map(|item| {item.to_string()}).collect();
+                all_touched_coordinates.insert(xy.join(","));
             }
         }
     }
 
 //    println!("{:?}", all_touched_coordinates);
     all_touched_coordinates
+}
+
+// fn 
+
+fn find_min<'a, I>(coordinates: I) -> i32
+where
+    I: Iterator<Item = &'a String>
+{
+    let mut min = 100000;
+    for coordinate in coordinates {
+
+        let total = coordinate.split(",").fold(0, |mut acc: i32, x: &str| {
+            acc += x.parse::<i32>().unwrap().abs();
+            acc
+        }); 
+
+
+        if total < min {
+            min = total;
+        }
+    }
+    min
 }
 
